efbaafcd636e913ddd99625458ef94023fad2ec1
Vladyslav <valdrudik@gmail.com>
2020-02-21 13:22:24 +0200

    MobV2.0

--- js/BarberShop.js
+++ js/BarberShop.js
@@ -93,3 +93,20 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 500);
     });
 });
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
+
+  $(window).on('load', function () {
+    $('body').addClass('loaded_hiding');
+    window.setTimeout(function () {
+      $('body').addClass('loaded');
+      $('body').removeClass('loaded_hiding');
+    }, 500);
+});
+
